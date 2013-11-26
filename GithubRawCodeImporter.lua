d5676afef109e1e8008407cd22663fba9a47fdf4
sytone <github@sytone.com>
2013-11-26 08:19:02 -0800

    Update GithubRawCodeImporter.lua
    Enabled overwrite.

--- GithubRawCodeImporter.lua
+++ GithubRawCodeImporter.lua
@@ -3,31 +3,36 @@
 -- Protected by GNU LGPL: http://bit.ly/17YzEWB
 
 local tArgs = {...}
-if #tArgs < 4 or #tArgs > 4 then
-  print("Usage: github <author> <repo> <path> <program>")
-  return
-end
-
-local author = tArgs[1]
-local repo = tArgs[2]
-local path = tArgs[3]
-local prog = tArgs[4]
-
-local getUrl = http.get(
-"https://raw.github.com/"..author.."/"..repo.."/"..path
-)
-
+local overwrite = false
 local errors = {}
 
+-- Validate the command line. If there are five we overwrite!
+if #tArgs < 4 then
+  print("Usage: github <author> <repo> <path> <program> overwrite")
+  return
+elseif #tArgs >= 4 then
+  local author = tArgs[1]
+  local repo = tArgs[2]
+  local path = tArgs[3]
+  local prog = tArgs[4]
+end
+
+if #tArgs = 5 then
+  overwrite = true
+end
+
+local getUrl = http.get("https://raw.github.com/"..author.."/"..repo.."/"..path)
+
 if not http then
-  table.insert(errors,"Requires HTTP API")
+  table.insert(errors,"Requires HTTP API.")
 end
 if not getUrl then
-  table.insert(errors,"Connection trouble")
+  table.insert(errors,"Connection trouble.")
 end
-if fs.exists(prog) then
-  table.insert(errors,"Program already exists")
+if fs.exists(prog) and not overwrite then
+  table.insert(errors,"Program already exists, not overwriting.")
 end
+
 if #errors > 0 then
   print("Failed! Errors detected:")
   for i = 1, #errors do
